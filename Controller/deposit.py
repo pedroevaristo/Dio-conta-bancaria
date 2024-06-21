083fcdc11eb1605319d73ca9f97332355e8b74c0
pedroeva <pedrodzprogramacao@gmail.com>
2024-06-20 22:53:04 -0300

    20/06/2024 22:52

--- Controller/deposit.py
+++ Controller/deposit.py
@@ -1,8 +1,20 @@
 #Factory Method
 from data.db import connection
+from data.db import Connection_With_Database
 from Model.bank_account import person
 from Model.interface import functions
 
-class depositing_In_Account(name, transaction):
+class depositing_In_Account(self):
+    new_Deposit = Connection_With_Database.connection()
+    print("Digite as informações para o deposito")
+
+    name_Person = input("Nome: ")
+
+    value_Deposit = input("Valor do deposito: ")
     
-    connection.Connection_With_Database
+    
+
+    if verifing_Depoist:
+        print("Depositado na sua conta")
+    else:
+        print("Houve um problema no deposito a sua conta")
--- data/db.py
+++ data/db.py
@@ -1,20 +1,25 @@
 
+from typing import Any
 from Model.bank_account import person
 class connection:#Singleton connection
     _instance = None
     _connection = None
-    
-    def __new__(cls, *args, **kwargs):
+    def __call__(self, *args, **kwds):
+
         if cls._instance is None:
-            cls._instance = super(connection, cls).__new__(cls, *args, **kwargs)
+            cls._instance = super(connection, cls).__call__(cls, *args, **kwargs)
         return cls._instance
     
-    def Connection_With_Database(self):
+class Connection_With_Database(metaclass=connection):
+   _connection=None
+   def connection(self):
         if not list_Account:
             self.Connection_With_Database = list_Account = []
+            self.Connection_With_Database = transactions_ = {}
         return self.Connection_With_Database
 
-
+    #def transfering_Money:
+        
 
     ''''def remove_added_Information(self, id):
         if id in self.size_list:
